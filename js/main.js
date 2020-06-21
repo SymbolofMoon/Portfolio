227c6f88bd8ba59e33e017bed56a45e14420c6af
prateekchandra <prateekchandra1027@gmail.com>
2020-06-21 23:28:03 +0530

    Final commit

--- js/main.js
+++ js/main.js
@@ -13,4 +13,49 @@ $(document).ready(function() {
 
         return false;
     })
+
+
+
+    $('.project-area .button-group #btn1').trigger('click');
+
+    $('.project-area .grid .test-popup-link').magnificPopup({
+        type: 'image',
+        gallery: {  enabled:true }
 })
+
+
+//Owl-Carausel
+
+$('.site-main .about-area .owl-carousel').owlCarousel({
+    loop: true,
+    autoplay:true,
+    dots:true,
+    responsive:{
+        0: {
+            items: 1
+        },
+        560: {
+            items: 2
+        }
+    }
+})
+
+let nav_offset_top = $('.header_area').height() + 50;
+
+    function navbarFixed() {
+        if ($('.header_area').length) {
+            $(window).scroll(function () {
+                let scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
+                    $('.header_area .main-menu').addClass('navbar_fixed');
+                } else {
+                    $('.header_area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+        }
+    }
+
+    navbarFixed();
+
+
+});
